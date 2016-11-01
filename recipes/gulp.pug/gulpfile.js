fc871747e777f66fff29bbd87b2e8d2cc7b40db4
Michelle Baert <rocky.road@rocky-shore.net>
2016-11-01 13:38:09 +0100

    Tried a simple solution. Causes TypeError: dest.on is not a function

--- recipes/gulp.pug/gulpfile.js
+++ recipes/gulp.pug/gulpfile.js
@@ -2,16 +2,30 @@ var gulp        = require('gulp');
 var browserSync = require('browser-sync');
 var sass        = require('gulp-sass');
 var pug         = require('gulp-pug');
+// var data        = require('gulp-data');
 var reload      = browserSync.reload;
+
+/*
+  Get data via JSON file
+*/
+
+var YOUR_LOCALS = {};// makeContext();
+
+gulp.task('context', function() {
+  return gulp.src('data/example.json')
+        .pipe(function(file, encoding, callback) {
+            console.log("Context changed:", file.content);
+            // YOUR_LOCALS = file.content;
+            callback();
+            return file;
+    });
+});
 
 /**
  * Compile pug files into HTML
  */
 gulp.task('templates', function() {
 
-  var YOUR_LOCALS = {
-    "message": "This app is powered by gulp.pug recipe for BrowserSync"
-  };
 
     return gulp.src('./app/*.pug')
         .pipe(pug({
@@ -39,11 +53,12 @@ gulp.task('sass', function () {
 /**
  * Serve and watch the scss/pug files for changes
  */
-gulp.task('default', ['sass', 'templates'], function () {
+gulp.task('default', ['sass', 'context', 'templates'], function () {
 
     browserSync({server: './dist'});
 
 
     gulp.watch('./app/scss/*.scss', ['sass']);
+    gulp.watch('./data/**/*.json', ['context', 'pug-watch']);
     gulp.watch('./app/*.pug',      ['pug-watch']);
 });
